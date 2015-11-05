756f7a2b6b73c1596566430428fe3c28031f5fe2
Ishan Shah <ishah@calstatela.edu>
2015-11-04 22:22:39 -0800

    Folder Structure Changes

--- src/MastcamModule/Ports.java
+++
@@ -1,6 +0,0 @@
-package MastcamModule;
-
-public class Ports {
-	public static final int MASTCAM_PORT = 9006;
-	public static final int COMPUTER_COMMAND_AND_DATA_SUBSYSTEM_PORT = 9009;
-}
--- src/callback/CallBack.java
+++
@@ -1,5 +0,0 @@
-package callback;
-
-public class CallBack {
-
-}
