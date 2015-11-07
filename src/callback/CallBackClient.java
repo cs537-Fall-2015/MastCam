ceed8df5833fbe2a96ddc094d548a9b8f26a5fee
Vikalp Patel <vikalp.patel007@gmail.com>
2015-11-06 21:30:38 -0800

    Callbackclient modified

--- src/callback/CallBackClient.java
+++ src/callback/CallBackClient.java
@@ -1,10 +1,42 @@
 package callback;
 
-public class CallBackClient {
+import java.io.ObjectOutputStream;
+import java.net.InetAddress;
+import java.net.UnknownHostException;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+import generic.RoverClientRunnable;
 
+public class CallBackClient extends RoverClientRunnable{
+
+	public CallBackClient(int port, InetAddress host)
+			throws UnknownHostException {
+		super(port, host);
+	}
+
+	@Override
+	public void run() {
+		try{
+			ObjectOutputStream outputToAnotherObject = null;
+		    
+            //write to socket using ObjectOutputStream
+            outputToAnotherObject = new ObjectOutputStream(getRoverSocket().getNewSocket().getOutputStream());
+            
+            System.out.println("Call Back 1 Client: Sending request to Control Server");
+
+            outputToAnotherObject.writeObject("CCDS_DONE");
+
+            //close resources
+            outputToAnotherObject.close();
+            closeAll();
+		}	        
+        catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		catch (Exception error) {
+			System.out.println("Client: Error:" + error.getMessage());
+		}
+		
 	}
 
 }
