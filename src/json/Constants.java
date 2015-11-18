4a08f064a2357acfc7ccef97d78a9a49c769200c
Vrushank <vrushank1405@gmail.com>
2015-11-18 12:16:32 -0800

    Changes in code

---
+++ src/json/Constants.java
@@ -0,0 +1,9 @@
+package json;
+
+public class Constants {
+	public static final int ONE = 1;
+	public static final int TWO = 2;
+	
+	public static final int PORT_ONE = 9001;
+	public static final int PORT_TWO = 9002;
+}
